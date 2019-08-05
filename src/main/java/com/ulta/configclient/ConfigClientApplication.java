7c3a3a56c76df676a130f69f490044c6bfea3e7b
Praval Kumar <Praval.Kumar@hcl.com>
2019-08-05 18:41:39 +0530

    minor changes

--- src/main/java/com/ulta/configclient/ConfigClientApplication.java
+++ src/main/java/com/ulta/configclient/ConfigClientApplication.java
@@ -3,6 +3,13 @@ package com.ulta.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Configuration client - 
+ * 		connects to Config Server
+ * @author Praval.Kumar
+ *
+ */
+
 @SpringBootApplication
 public class ConfigClientApplication {
 
--- src/main/java/com/ulta/configclient/MessageResource.java
+++ src/main/java/com/ulta/configclient/MessageResource.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+/**
+ * ConfigClient - RestController 
+ * @author Praval.Kumar
+ */
 
 @RefreshScope
 @RestController
