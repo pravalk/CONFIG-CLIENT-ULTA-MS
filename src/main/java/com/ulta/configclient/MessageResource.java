48b04ea9912dc706568edbf4fba2c559ef2ba8a0
Praval Kumar <Praval.Kumar@hcl.com>
2019-08-05 18:22:26 +0530

    Config-Server : changes complete

--- src/main/java/com/ulta/configclient/MessageResource.java
+++ src/main/java/com/ulta/configclient/MessageResource.java
@@ -3,21 +3,24 @@ package com.ulta.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
-@RequestMapping("/rest")
 @RestController
 public class MessageResource {
 	
-	@Value("${message: Default hello}")
+	@Value("${no.data}")
 	private String message;
+	
+	@Value("${error.code}")
+	private String errorCode;
 	
 	@GetMapping("/message")
 	public String message(){
 		
-		return message;
+		return new StringBuilder().
+					append(message).
+					toString();
 	}
 
 }
